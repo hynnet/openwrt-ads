1cc826978b3156256bc00ea5ea62b306f92e1086
hynnet <hynnet@gmail.com>
2014-09-14 22:14:57 +0800

    修正安装命令说明的错误。

--- install-adblock.sh
+++ install-adblock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # install-adblock.sh by hynnet
 # download and install command:
-# curl -k 'https://raw.githubusercontent.com/hynnet/openwrt-ads/master/install-adblock.sh -o /tmp/install-adblock.sh ; chmod +x /tmp/install-adblock.sh ; /tmp/install-adblock.sh
+# curl -k 'https://raw.githubusercontent.com/hynnet/openwrt-ads/master/install-adblock.sh' -o /tmp/install-adblock.sh ; chmod +x /tmp/install-adblock.sh ; /tmp/install-adblock.sh
 
 curl -k 'https://raw.githubusercontent.com/hynnet/openwrt-ads/master/dnsmasq.ads' -o /etc/dnsmasq.ads
 grep conf-file /etc/dnsmasq.conf || echo -e "\nconf-file=/etc/dnsmasq.ads" >> /etc/dnsmasq.conf
